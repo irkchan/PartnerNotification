3366600b9bb235b1973a7729091ddc9ea7017191
shino8_0921 <ntm10107@gmail.com>
2021-11-08 12:48:20 +0900

    Initial Commit

---
+++ PartnerNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartnerNotification
+//
+//  Created by 吉田なつみ on 2021/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
