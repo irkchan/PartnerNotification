fd4d10968599a7dec3b9f43a2070852893f061f0
shino8_0921 <ntm10107@gmail.com>
2021-11-09 09:11:40 +0900

    ・pod installと.gitignoreの作成 ・story boardにview controllerを追加

---
+++ PartnerNotification/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  PartnerNotification
+//
+//  Created by 吉田なつみ on 2021/11/08.
+//
+
+import UIKit
+
+class HomeViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '14.5'
+
+target 'PartnerNotification' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PartnerNotification
+pod 'RealmSwift','10.11.0'
+  pod 'Firebase','8.4.0'
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+  pod 'Firebase/Storage'
+  pod 'FirebaseUI/Storage'
+  pod 'FanMenu'
+  
+end
